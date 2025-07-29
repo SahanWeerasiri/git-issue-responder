502559592b4f7c4ca31c23e052242ddbd556a717
sahanweerasiri <smudunlahiru@gmail.com>
2025-07-29 15:40:48 +0000

    Fix: [CUSTOM] Need to update the response given by the / url path
    Fix the response message to show something cool

--- project/main.py
+++ project/main.py
@@ -1,4 +1,3 @@
-```python
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Updated the response message to be more informative and user-friendly
-    return 'Welcome to the Flask application! This is the home page. You can explore other routes to see more.'
+    # Updated the response to be "something cool"
+    return 'Behold! This is the home page. Flask is pretty awesome!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
